c6e62e0f0749bd4eee616032874e639cade59ce1
Jhenny Mercado <jmercadoa2001@gmail.com>
2025-08-03 23:04:39 -0400

    Metodo CRUD de clientes y autenticacion

--- backend/app/Http/Controllers/clienteController.php
+++ backend/app/Http/Controllers/clienteController.php
@@ -24,6 +24,20 @@ class clienteController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'nombre' => 'required|string|max:50',
+            'apellidos' => 'required|string|max:50',
+            'direccion' => 'required|string|max:150',
+            'celular' => 'required|string|max:50',
+            'nit' => 'required|string|max:20',
+        ]);
+    
+        $cliente = cliente::create($request->all());
+    
+        return response()->json([
+            'mensaje' => 'Cliente registrado exitosamente',
+            'cliente' => $cliente
+        ], 201);
     }
 
     /**
@@ -32,6 +46,13 @@ class clienteController extends Controller
     public function show(string $id)
     {
         //
+        $cliente = cliente::find($id);
+
+    if (!$cliente) {
+        return response()->json(['mensaje' => 'Cliente no encontrado'], 404);
+    }
+
+    return response()->json($cliente, 200);
     }
 
     /**
@@ -40,6 +61,26 @@ class clienteController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $cliente = cliente::find($id);
+
+    if (!$cliente) {
+        return response()->json(['mensaje' => 'Cliente no encontrado'], 404);
+    }
+
+    $request->validate([
+        'nombre' => 'required|string|max:50',
+        'apellidos' => 'required|string|max:50',
+        'direccion' => 'required|string|max:150',
+        'celular' => 'required|string|max:50',
+        'nit' => 'required|string|max:20',
+    ]);
+
+    $cliente->update($request->all());
+
+    return response()->json([
+        'mensaje' => 'Cliente actualizado exitosamente',
+        'cliente' => $cliente
+    ], 200);
     }
 
     /**
@@ -48,5 +89,14 @@ class clienteController extends Controller
     public function destroy(string $id)
     {
         //
+        $cliente = cliente::find($id);
+
+    if (!$cliente) {
+        return response()->json(['mensaje' => 'Cliente no encontrado'], 404);
+    }
+
+    $cliente->delete();
+
+    return response()->json(['mensaje' => 'Cliente eliminado exitosamente'], 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\categoriaController;
 use App\Http\Controllers\clienteController;
+use App\Http\Controllers\AuthController;
+
  
 
 Route::apiResource('categorias', categoriaController::class);
 Route::apiResource('clientes', clienteController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
